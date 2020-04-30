f5e88f818765992b0562a50437be9811561e6c67
Lina Ebeid <linaebe0@gmail.com>
2020-04-30 11:50:15 +0300

    format file

--- quiz/src/components/QuizForm.js
+++ quiz/src/components/QuizForm.js
@@ -1,8 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Quizform({ questions }) {
-  const [answer, setAnswer] = useState('');
-  const [answers, setAnswers] = useState(['', '', '', '', '', '', '', '', '', '']);
+  const [answer, setAnswer] = useState("");
+  const [answers, setAnswers] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
   const [result, setResult] = useState(0);
   const [display, setDisplay] = useState(true);
   // const [background, setBackground] = useState('white');
@@ -10,56 +21,71 @@ function Quizform({ questions }) {
   useEffect(() => {
     for (let i = 0; i <= 10; i++) {
       if (document.querySelector(`.questions${i}:checked`)) {
-        document.querySelector(`.questions${i}:checked`).parentNode.style.background = 'red';
+        document.querySelector(
+          `.questions${i}:checked`
+        ).parentNode.style.background = "red";
       }
     }
-  }, [answers])
+  }, [answers]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setDisplay(false);
-    setResult(answers.filter(e => e === 'correct').length);
-  }
+    setResult(answers.filter((e) => e === "correct").length);
+  };
 
   const handleChange = (e, index) => {
     setAnswer(e.target.value);
-    setAnswers([...answers.slice(0, index),
-    e.target.value,
-    ...answers.slice(index + 1)]);
+    setAnswers([
+      ...answers.slice(0, index),
+      e.target.value,
+      ...answers.slice(index + 1),
+    ]);
     // document.querySelector(`.inputcontainer:focus-within`).style.background='white';
 
     // document.querySelector(`.questions${index}:checked`).parentNode.style.background='red';
     // setBackground('red')
-
-  }
+  };
   return (
     <>
-      {display &&
-        <form onSubmit={handleSubmit} style={{ display }}>
+      {display && (
+        <form onSubmit={handleSubmit} >
           {questions.map((e, index) => (
             <div key={index}>
               <p>{e.question}</p>
 
               <div class="answers">
-
-                <label class="inputcontainer"  >
-                  <input className={`questions${index}`} type="radio" name={`answer${index}`} value="correct" onChange={(e) => handleChange(e, index)} />
+                <label class="inputcontainer">
+                  <input
+                    className={`questions${index}`}
+                    type="radio"
+                    name={`answer${index}`}
+                    value="correct"
+                    onChange={(e) => handleChange(e, index)}
+                  />
                   {e.correct_answer}
                 </label>
-                {e.incorrect_answers.map(incorrect => <label class="inputcontainer">
-                  <input className={`questions${index}`} type="radio" name={`answer${index}`} value="incorrect" onChange={(e) => handleChange(e, index)} />
-                  {incorrect}
-                </label>)}
+                {e.incorrect_answers.map((incorrect) => (
+                  <label class="inputcontainer">
+                    <input
+                      className={`questions${index}`}
+                      type="radio"
+                      name={`answer${index}`}
+                      value="incorrect"
+                      onChange={(e) => handleChange(e, index)}
+                    />
+                    {incorrect}
+                  </label>
+                ))}
               </div>
-
-            </div>))}
-          <input type="submit" value="Submit" className="submitBtn"/>
-
+            </div>
+          ))}
+          <input type="submit" value="Submit" className="submitBtn" />
         </form>
-      }
+      )}
       {!display && <div> Your Result {result}</div>}
     </>
-  )
+  );
 }
 
 export default Quizform;
